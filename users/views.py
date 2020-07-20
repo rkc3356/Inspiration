2642c0f139cbfd127ac99e42cb0dcf3c2ec9901f
Raavi Chowdhury <rkc3356@gmail.com>
2020-07-19 22:29:30 -0400

    Created log out template.

--- users/views.py
+++ users/views.py
@@ -8,8 +8,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request,f'Account Created for: {username}!')
-            return redirect('inspiration')
+            messages.success(request,f'Your account as been created, log in!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
